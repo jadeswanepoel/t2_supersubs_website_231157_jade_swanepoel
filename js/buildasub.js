7ec243f3ab88e0f200a964870bce2a01b9469344
jadeswanepoel <125471787+jadeswanepoel@users.noreply.github.com>
2023-06-13 10:36:04 +0200

    working on the javascript functionality

--- js/buildasub.js
+++ js/buildasub.js
@@ -1,18 +1,85 @@
-let sandwichOrder = [];
+let subOrder = [];
 
-makeSandwich = () =>{
+generateSub = () =>{
     let sandwichTotal = 0;
 
-    let sandwichName = document.getElementById("sandwichName").value;
-    let size = document.getElementById("size").value;
+    let subName = document.getElementById("subName").value;
 
-    if(size === "Small"){
-        sandwichTotal = sandwichTotal + 20;
-    } else if(size === "Medium"){
-       sandwichTotal = sandwichTotal + 40;
-    } else if(size === "Large"){
-        sandwichTotal = sandwichTotal + 60;
+    let breadOptions = document.getElementsByName("breadRadio");
+    let breadValue;
+    for(let i = 0; i < breadOptions.length; i++ ){
+        if(breadOption[i].checked){
+            breadValue = baseOption[i].value
+            subTotal = subTotal + +breadOption[i].dataset.cost
+        }
     }
 
-    console.log(sandwichOrder)
+    let toppingOptions = document.getElementsByName("toppings");
+    let topArray = [];
+    for(let i = 0; i < toppingOptions.length; i++){
+        if(toppingOptions[i].checked){
+            topArray.push(toppingOptions[i].value);
+            subTotal = subTotal + +toppingOptions[i].dataset.cost
+        }
+    }
+
+    let sauceOptions = document.getElementsByName("sauceRadio");
+    let sauceValue;
+    for(let i = 0; i < sauceOptions.length; i++ ){
+        if(sauceOption[i].checked){
+            sauceValue = baseOption[i].value
+            subTotal = subTotal + +sauceOption[i].dataset.cost
+        }
+    }
+
+  
+
+
+    subOrder.push({
+        subName: subName,
+        subBread: breadValue,
+        subToppings: topArray,
+        subSauce:sauceValue,
+        subPrice: subTotal
+    });
+
+    console.log(subOrder)
+
+    document.getElementById("realTimeCost").innerHTML = "R0.00"
+
+    document.getElementById("subForm").reset();
+
+}
+
+realTimeCost = ( )  => {
+
+    realTimePrice = 0;
+
+
+
+    let breadOption = document.getElementsByName("breadRadio")
+    for(let i = 0; i < baseOption.length; i++){
+        if(breadOption[i].checked){
+           
+           realTimePrice = realTimePrice + +breadOption[i].dataset.cost
+        }
+    }
+
+    let toppingOptions = document.getElementsByName("toppings");
+    for(let i = 0; i < toppingOptions.length; i++){
+        if(toppingOptions[i].checked){
+            
+            realTimePrice = realTimePrice + +toppingOptions[i].dataset.cost
+        }
+    }
+
+    let sauceOptions = document.getElementsByName("toppings");
+    for(let i = 0; i < sauceOptions.length; i++){
+        if(sauceOptions[i].checked){
+            
+            realTimePrice = realTimePrice + +sauceOptions[i].dataset.cost
+        }
+    }
+
+    document.getElementById("realTimeCost").innerHTML = "R" + realTimePrice + ".00"
 }
